4fb4cae0b04afc86e655389a0469ca4cce12e1e6
Paolo Fragomeni <paolo@async.ly>
2015-05-03 12:28:12 +0200

    handle err before assigning the fd

--- main.go
+++ main.go
@@ -15,21 +15,20 @@ type size struct {
 func TermSize() (error, int, int) {
 
 	var sz size
-	var fp *os.File
-	var fh int
 	var fd uintptr
-	var err error
 
 	if runtime.GOOS == "windows" {
-		fh, err = syscall.Open("CONOUT$", syscall.O_RDWR, 0)
-		fd = uintptr(fh)
+		if fh, err := syscall.Open("CONOUT$", syscall.O_RDWR, 0); err != nil {
+			return err, int(0), int(0)
+		} else {
+			fd = uintptr(fh)
+		}
 	} else {
-		fp, err = os.OpenFile("/dev/tty", syscall.O_WRONLY, 0)
-		fd = fp.Fd()
-	}
-
-	if err != nil {
-		return err, int(0), int(0)
+		if fp, err := os.OpenFile("/dev/tty", syscall.O_WRONLY, 0); err != nil {
+			return err, int(0), int(0)
+		} else {
+			fd = fp.Fd()
+		}
 	}
 
 	_, _, _ = syscall.Syscall(
